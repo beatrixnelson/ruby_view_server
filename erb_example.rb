36648b6b8e497d860a7d29e1255ef18c2cf43a2a
User <Macintosch@Amys-MacBook-Air.local>
2015-05-19 13:42:42 +0200

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,15 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+shoes = ['converse', 'flipflops', 'crocs', 'boots', 'heels']
+shoes.each do |shoe|
+	puts 'I love my #{shoe}'
+end
+
+a = 31
+b = 33
+puts a+b
+
+name = Amy
+puts 'My name is #{name}.'
